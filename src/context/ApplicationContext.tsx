1bb9e473fda68b16bf68a724e08e109b8c3856f4
gimefaya <flojuve@hotmail.fr>
2026-06-25 14:19:16 +0200

    feat : global context created

---
+++ src/context/ApplicationContext.tsx
@@ -0,0 +1,61 @@
+import { createContext, useState, useContext } from 'react';
+import type { Application } from '../types/application';
+import { mockApplications } from '../datas/mockApplications';
+
+interface ApplicationContextType {
+  applications: Application[];
+  addApplication: (newApp: Omit<Application, 'id'>) => void;
+  removeApplication: (id: number) => void;
+  updateApplication: (updateApp: Application) => void;
+}
+
+const ApplicationContext = createContext<ApplicationContextType | null>(null);
+
+interface ApplicationProviderProps {
+  children: React.ReactNode;
+}
+
+export default function ApplicationProvider({
+  children,
+}: ApplicationProviderProps) {
+  const [applications, setApplications] =
+    useState<Application[]>(mockApplications);
+
+  function addApplication(newApp: Omit<Application, 'id'>) {
+    const appWithId: Application = { ...newApp, id: Date.now() };
+    setApplications([...applications, appWithId]);
+  }
+
+  function removeApplication(id: number) {
+    setApplications(applications.filter((app) => app.id !== id));
+  }
+
+  function updateApplication(updateApp: Application) {
+    setApplications(
+      applications.map((app) => (app.id === updateApp.id ? updateApp : app)),
+    );
+  }
+
+  return (
+    <ApplicationContext.Provider
+      value={{
+        applications,
+        addApplication,
+        removeApplication,
+        updateApplication,
+      }}
+    >
+      {children}
+    </ApplicationContext.Provider>
+  );
+}
+
+export function useApplications() {
+  const context = useContext(ApplicationContext);
+  if (context === null) {
+    throw new Error(
+      'useApplications doit être utilisé dans un ApplicationProvider',
+    );
+  }
+  return context;
+}
